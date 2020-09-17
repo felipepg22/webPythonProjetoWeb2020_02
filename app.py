716b9fd9fa6ee94e21a4aa0e14b20313fd1d6925
Felipe <felipepg@uniplaclages.edu.br>
2020-09-17 11:14:37 -0300

    Adicionado form pedido close #4

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template
 from mod_cliente.cliente import bp_cliente
 from mod_produto.produto import bp_produto
 from mod_home.home import bp_home
+from mod_pedido.pedido import bp_pedido
 
 app = Flask(__name__)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_produto)
 app.register_blueprint(bp_home)
+app.register_blueprint(bp_pedido)
 
 if __name__ == "__main__":
     app.run()
