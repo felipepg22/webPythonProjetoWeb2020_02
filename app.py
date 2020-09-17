e70040c0293a4b80db3b96146a938052089a2651
Felipe <felipepg@uniplaclages.edu.br>
2020-09-17 10:22:21 -0300

    Criado form produto close #3

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #coding utf-8
 from flask import Flask, render_template
 from mod_cliente.cliente import bp_cliente
+from mod_produto.produto import bp_produto
 
 app = Flask(__name__)
 app.register_blueprint(bp_cliente)
+app.register_blueprint(bp_produto)
 
 @app.route("/")
 def index():
