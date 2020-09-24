6eea8b9ebe7d132d91505f06f8a82267ffde6857
Felipe <felipepg@uniplaclages.edu.br>
2020-09-23 21:29:29 -0300

    Login criado

--- mod_login/login.py
+++ mod_login/login.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 bp_login = Blueprint('login', __name__, url_prefix='/', template_folder='templates')
 
-@bp_login.route("/l")
+@bp_login.route("/")
 def login():
     return render_template('formLogin.html')
 
@@ -14,7 +14,7 @@ def validaLogin():
 
     if _name == 'abc' and _pass == 'Bolinhas':
         #abre a aplicação na tela home
-        return redirect(url_for('home.home'))
+        return redirect(url_for('home.index'))
     else:
         #retorna para a tela de login
         return redirect(url_for('login.login', falhaLogin=1))
