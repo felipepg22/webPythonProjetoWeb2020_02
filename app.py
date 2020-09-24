a742495a41076582c069df9945ee85be269a4c1f
Felipe <felipepg@uniplaclages.edu.br>
2020-09-24 10:49:34 -0300

    Criado o tempo para a sessão

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #coding utf-8
 from flask import Flask, render_template, session
 import os
+from datetime import timedelta
 from mod_login.login import bp_login
 from mod_cliente.cliente import bp_cliente
 from mod_produto.produto import bp_produto
@@ -12,6 +13,11 @@ app = Flask(__name__)
 
 #cria uma chave randomica para a sessão
 app.secret_key = os.urandom(12).hex()
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
 
 app.register_blueprint(bp_login)
 app.register_blueprint(bp_cliente)
