1b4ce77e36d9cc1e769564d90e178c131ab303f0
Felipe <felipepg@uniplaclages.edu.br>
2020-10-07 20:54:51 -0300

    close #34

---
+++ BancoBD.py
@@ -0,0 +1,9 @@
+import pymysql
+
+class Banco():    
+    def __init__(self):
+        host = "127.0.0.1"
+        user = "felipe"
+        password = "fepaga221292"
+        db = "db_abc_bolinhas"
+        self.conexao = pymysql.connect(host, user, password, db)
